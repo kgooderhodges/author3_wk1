9164401b575023ba38add5a93817dcf5a75a589e
Katie <katehodges@me.com>
2020-10-29 16:01:47 -0400

    starting the f again

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// import your packages here
+import { fetchData, postData } from "./modules/DataMiner.js";
+
+(() => {
+    // stub * just a place for non-component-specific stuff
+    console.log('loaded');
+    
+    function popErrorBox(message) {
+        alert("Something has gone horribly, horribly wrong");
+    }
+
+    function handleDataSet(data) {
+        let userSection = document.querySelector('.user-section'),
+            userTemplate = document.querySelector('#profs-template').content;
+
+        for (let user in data) {
+            let currentUser = userTemplate.cloneNode(true),
+                currentUserText = currentUser.querySelector('.user').children;
+
+            currentUserText[1].src = `images/${data[user].Avatar}`;
+            currentUserText[2].textContent = data[user].Name;
+            currentUserText[3].textContent = data[user].Role;
+            currentUserText[4].textContent = data[user].Nickname;
+
+            // add this new user to the view
+            userSection.appendChild(currentUser);
+        }
+    }
+
+    
+    // we can add a catch handler to a thenable if things go wrong during our data retrieval attempt
+    // really, we should move all of this to an external class or function and pass arguments into it.
+
+    // that would make it really flexible and able to handle all kinds of requests and we could pass in a callback depending on what we want to do with our data
+
+    // but then we'd be on our way to rewriting the Axios API (you should research it)
+    fetchData("./includes/functions.php").then(data => handleDataSet(data)).catch(err => { console.log(err); popErrorBox(err); });
+})();
